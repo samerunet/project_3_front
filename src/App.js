c34df998886909a8eadbba6dd7bf30e04276a306
samerunet <samerunet@gmail.com>
2022-08-30 12:56:37 -0700

    idk

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ function App() {
 	}, []);
 
 	const handleUpdate = (Data) => {
+		Data.preventDefault();
 		axios
 			.put(`https://young-oasis-10029.herokuapp.com/update/${Data._id}`, {
 				username: username,
@@ -165,6 +166,59 @@ function App() {
 							>
 								Delete
 							</button>
+							<div className='updateForm'>
+								<form className='form-control' onSubmit={handleUpdate}>
+									username:
+									<input
+										className='form-control'
+										placeholder={post.username}
+										type='text'
+										onChange={handleNewUser}
+									/>
+									<br />
+									image:
+									<input
+										className='form-control'
+										placeholder={post.image}
+										type='text'
+										onChange={handlesetImage}
+									/>
+									<br />
+									video:
+									<input
+										className='form-control'
+										placeholder={post.video}
+										type='text'
+										onChange={handlesetVideo}
+									/>
+									<br />
+									comment:
+									<input
+										className='form-control'
+										placeholder={post.comment}
+										type='text'
+										onChange={handlesetComment}
+									/>
+									<br />
+									likes:
+									<input
+										className='form-control'
+										type='number'
+										placeholder={post.likes}
+										onChange={handlesetLikes}
+									/>
+									<br />
+									caption:
+									<input
+										className='form-control'
+										placeholder={post.caption}
+										type='text'
+										onChange={handlesetCaptions}
+									/>
+									<br />
+									<input type='submit' value='edit button' />
+								</form>
+							</div>
 						</div>
 					);
 				})}
