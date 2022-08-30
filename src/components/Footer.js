6a11d5adbedf6f138743907ad3849b5bd59f3aa1
eduardopaaaa <eddy.nator@hotmail.com>
2022-08-30 14:23:09 -0600

    front end comp changes

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+	return (
+		<div className='post'>
+			<h2> Post: {props.libraries}</h2>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+	return (
+		<div className='header'>
+			<h2> Post: {props.libraries}</h2>
+		</div>
+	);
+};
+
+export default Header;
