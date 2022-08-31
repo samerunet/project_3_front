4dcf904b8fd2d8403a861703d5247a09197aefa5
samerunet <samerunet@gmail.com>
2022-08-30 18:39:28 -0700

    routes changed to patch 4 update

--- src/App.js
+++ src/App.js
@@ -106,7 +106,6 @@ function App() {
 
 	return (
 		<div className='App'>
-			<Header />
 			<div>
 				<br />
 				<br />
@@ -178,70 +177,10 @@ function App() {
 							>
 								Delete
 							</button>
-
-							<div className='updateForm'>
-								<form
-									className='form-control'
-									onSubmit={(event) => {
-										handleUpdate(post);
-									}}
-								>
-									username:{""}
-									<input
-										className='form-control'
-										placeholder={post.username}
-										type='text'
-										onChange={handleNewUser}
-									/>
-									<br />
-									image:{""}
-									<input
-										className='form-control'
-										placeholder={post.image}
-										type='text'
-										onChange={handlesetImage}
-									/>
-									<br />
-									video:{""}
-									<input
-										className='form-control'
-										placeholder={post.video}
-										type='text'
-										onChange={handlesetVideo}
-									/>
-									<br />
-									comment:{""}
-									<input
-										className='form-control'
-										placeholder={post.comment}
-										type='text'
-										onChange={handlesetComment}
-									/>
-									<br />
-									likes:{""}
-									<input
-										className='form-control'
-										type='number'
-										placeholder={post.likes}
-										onChange={handlesetLikes}
-									/>
-									<br />
-									caption:{""}
-									<input
-										className='form-control'
-										placeholder={post.caption}
-										type='text'
-										onChange={handlesetCaptions}
-									/>
-									<br />
-									<input type='submit' value='edit button' />
-								</form>
-							</div>
 						</div>
 					);
 				})}
 			</ul>
-		
 		</div>
 	);
 }
