acbbd808884034bb481d7488240d77dcb0ba8e11
samerunet <samerunet@gmail.com>
2022-09-01 07:37:18 -0700

    story line added

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 
 export default function Story({ data }) {
 	return (
-		<>
+		<div className='storyContainer'>
+			<div classname='storyNothing'></div>
 			{data.map((item) => {
-				return <div>{item.img}</div>;
+				return (
+					<div className='story'>
+						<img src={item.image} alt={item.title} />
+						<p>{item.username}</p>
+					</div>
+				);
 			})}
-		</>
+		</div>
 	);
 }
