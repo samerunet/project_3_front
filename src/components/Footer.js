91226935e0eac81b563e9fe9c1dd89367268ce67
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-01 13:04:42 -0600

    pushing css changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,28 +2,9 @@ const Footer = (props) => {
 	return (
 		<div class='nav nav-masthead justify-content-center float-md'>
 			<footer class='mt-auto text-white-50'>
-			<p>FINSTAGRAM</p>
-				<a
-							className='nav-link  float-md-end mt-auto text-white-50 '
-							href='https://www.linkedin.com/in/sami-sipahi/'
-							target='_blank'
-							type='button'
-							rel='noreferrer'
-						>
-							{" "}
-							<p>SAMI SIPAHI</p>
-						</a>
+			<p>FINSTAGRAM | Designed in California | Made in China</p>
+				
 
-				<a
-							className='nav-link  float-md-start mt-auto text-white-50 '
-							href='https://www.linkedin.com/in/jesus-eduardo-prado/'
-							target='_blank'
-							type='button'
-							rel='noreferrer'
-						>
-							{" "}
-							<p>EDUARDO PRADO</p>
-						</a>
 						
 				
 			</footer>
