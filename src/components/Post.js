9bd60d0bc8c31143186215b8872b6d88bfcd597b
samerunet <samerunet@gmail.com>
2022-08-31 19:55:55 -0700

    pushing css changes

--- src/components/Post.js
+++ src/components/Post.js
@@ -19,93 +19,100 @@ export default function Post({
 	};
 	return (
 		<div className='postImage' key={post._id}>
-			<img src={post.image} alt={post.username} />
-			<button
-				onClick={(event) => {
-					handleDelete(post);
-				}}
-			>
-				Delete
-			</button>
-			<button
-				onClick={() => {
-					cardToggle(post);
-				}}
-			>
-				toggle edit
-			</button>
-
-			<div
-				id={`updateID${post._id}`}
-				className={`updateForm ${editFunction ? "" : "hide"}`}
-			>
-				<form
-					className='form-control'
-					onSubmit={(event) => {
-						event.preventDefault();
-						handleUpdate(post);
-					}}
-				>
-					username:{""}
-					<input
-						className='form-control'
-						placeholder={post.username}
-						type='text'
-						onChange={handleNewUser}
-					/>
-					<br />
-					image:{""}
-					<input
-						className='form-control'
-						placeholder={post.image}
-						type='text'
-						onChange={handlesetImage}
-					/>
-					<br />
-					video:{""}
-					<input
-						className='form-control'
-						placeholder={post.video}
-						type='text'
-						onChange={handlesetVideo}
-					/>
-					<br />
-					comment:{""}
-					<input
-						className='form-control'
-						placeholder={post.comment}
-						type='text'
-						onChange={handlesetComment}
-					/>
-					<br />
-					likes:{""}
-					<input
-						className='form-control'
-						type='number'
-						placeholder={post.likes}
-						onChange={handlesetLikes}
-					/>
-					<br />
-					caption:{""}
-					<input
-						className='form-control'
-						placeholder={post.caption}
-						type='text'
-						onChange={handlesetCaptions}
-					/>
-					<br />
-					<input type='submit' value='edit button' />
-				</form>
+			<div className='caption'>
+				<p>{post.caption}</p>
 			</div>
-			<div className='likeBox'>
-				{" "}
-				<p
-					className='like fa fa-heart'
-					onClick={() => {
-						likesIncrease(post);
-					}}
-				></p>
-				<p>{post.likes}</p>{" "}
+			<img src={post.image} alt={post.username} />
+			<div className='comments'>
+				<p>{post.comment}</p>
+			</div>
+			<div className='interactions'>
+				<div className='likeBox'>
+					{" "}
+					<p
+						className='like fa fa-heart'
+						onClick={() => {
+							likesIncrease(post);
+						}}
+					></p>
+					<p className='likeClikced'>{post.likes}</p>{" "}
+				</div>
+				<div className='editDelete'>
+					<p
+						className='fa fa-edit'
+						onClick={() => {
+							cardToggle(post);
+						}}
+					></p>
+					<p
+						className='fa fa-remove'
+						onClick={(event) => {
+							handleDelete(post);
+						}}
+					></p>
+				</div>
+				<div
+					id={`updateID${post._id}`}
+					className={`updateForm ${editFunction ? "" : "hide"}`}
+				>
+					<form
+						className='form-control'
+						onSubmit={(event) => {
+							event.preventDefault();
+							handleUpdate(post);
+						}}
+					>
+						username:{""}
+						<input
+							className='form-control'
+							placeholder={post.username}
+							type='text'
+							onChange={handleNewUser}
+						/>
+						<br />
+						image:{""}
+						<input
+							className='form-control'
+							placeholder={post.image}
+							type='text'
+							onChange={handlesetImage}
+						/>
+						<br />
+						video:{""}
+						<input
+							className='form-control'
+							placeholder={post.video}
+							type='text'
+							onChange={handlesetVideo}
+						/>
+						<br />
+						comment:{""}
+						<input
+							className='form-control'
+							placeholder={post.comment}
+							type='text'
+							onChange={handlesetComment}
+						/>
+						<br />
+						likes:{""}
+						<input
+							className='form-control'
+							type='number'
+							placeholder={post.likes}
+							onChange={handlesetLikes}
+						/>
+						<br />
+						caption:{""}
+						<input
+							className='form-control'
+							placeholder={post.caption}
+							type='text'
+							onChange={handlesetCaptions}
+						/>
+						<br />
+						<input type='submit' value='edit button' />
+					</form>
+				</div>
 			</div>
 		</div>
 	);
